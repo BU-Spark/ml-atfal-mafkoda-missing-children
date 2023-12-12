af8a14c68196642c3a7b872650e5822404ccacd8
mahaveer <37685-mahaveer@users.noreply.gitlab.aicrowd.com>
2023-12-12 18:19:51 -0500

    add code explaining comments
    Co-authored-by: Varshith Nukala <saisurya@bu.edu>

--- fall23/deployment/app.py
+++ fall23/deployment/app.py
@@ -4,14 +4,19 @@ import os
 import collections
 
 # Define the model metric variable
+
+# change this as per the generated csv result file 
 model_metric = 'VGG-Face_euclidean_l2'
+
+# column names
 missing_persons  = 'missing_filename'
 unknown_matches = 'unknowns_matched_filenames'
 
+# set title for the navigation bar 
 st.title("Prediction Visualizer")
 nav = st.sidebar.radio("Navigation", ['Visualize results'])
 
-# accept csv file as input
+# accept csv file as input in the sidebar
 uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
@@ -20,6 +25,7 @@ else:
     st.stop()
 df = data
 
+# display all the images in a grid given image paths and captions 
 def display_images_in_grid(image_paths, captions):
     if len(image_paths)==0:
         st.write("No images were captured")
@@ -38,6 +44,8 @@ def display_images_in_grid(image_paths, captions):
  
 if nav == 'Visualize results':
     def process_data(user_input):
+        # capture all the cases matching the (user_input) and combine all the match results (filtered_df)
+        # and then sort them and return res containing [ (image, distance_measure score),() ..... ]
         filtered_df = df[df[missing_persons].str.contains(user_input, na=False)]
         st.write()
         matches = [x.split(", ") for x in filtered_df[unknown_matches]]
@@ -52,6 +60,8 @@ if nav == 'Visualize results':
         res.sort(key=lambda x: x[1])
         return filtered_df,res
 
+    # crafting a seperate display result inorder to update the results
+    # if there is a change in range of the distance metric
     def display_results():
         start_th = st.session_state.threshold[0]
         end_th = st.session_state.threshold[1]
@@ -64,16 +74,22 @@ if nav == 'Visualize results':
                 captions.append(str("{:.2f}".format(th_val)))
         display_images_in_grid(image_paths, captions)
     
+    
     user_input = st.text_input("Enter a string to match:")
+
+    # if the slider changes reruns the function display_results to update the images shown
     threshold = st.select_slider("Enter value range of metric", key="threshold", options=[x * 0.01 for x in range(0, 101)], value=(0.2, 0.8), on_change = display_results)
 
+    # display info about metric and model
     st.divider()
     st.write("Model : ",model_metric.split("_",1)[0])
     st.write("Metric:   ",model_metric.split("_",1)[1])
     st.divider()
     
+    # if checked show uploaded csv file
     if st.checkbox("Show Uploaded CSV File"):
         st.table(data)
+
 
     if user_input:
         filtered_df, results_filtered = process_data(user_input)
@@ -85,6 +101,7 @@ if nav == 'Visualize results':
                 captions.append(image_path.rsplit("/")[-1])
             display_images_in_grid(image_paths, captions)
 
+        # see through results in a json format in browser
         if st.checkbox("Show Processed results"):
             st.write("Filtered Results: ", results_filtered)
     
